935fff39cbf6a8fb7395e225580ac5e9e33d1925
ANIeCaps <aniec.diritti.difesa@gmail.com>
2026-04-24 22:07:44 +0200

    Delete js-rete.js

--- js-rete.js
+++
@@ -1,8 +0,0 @@
-function check(){
-  let c = document.getElementById("code").value;
-  if(c === "!QAZAnIeC=OKM"){
-    document.getElementById("area").style.display="block";
-  } else {
-    alert("Codice errato");
-  }
-}
