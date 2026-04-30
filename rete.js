3bacd4b95fe63c89fb49ee167e42f3f77f63750f
ANIeCaps <aniec.diritti.difesa@gmail.com>
2026-04-30 18:26:25 +0200

    Delete rete.js

--- rete.js
+++
@@ -1,1 +0,0 @@
-function checkCode(){const code=document.getElementById('accessCode').value.trim();const error=document.getElementById('error');if(code==='!QAZAnIeC=OKM'){document.getElementById('locked').classList.add('hidden');document.getElementById('reserved').classList.remove('hidden');}else{error.textContent='Codice non valido. Richiedi autorizzazione allo staff ANI&C APS.';}}
